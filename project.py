5a76a09b190bcd3120f1928f179552610f080647
Abdallah Ismaili <89218851+AbdallahIsmaili@users.noreply.github.com>
2022-03-03 22:46:41 +0000

    start writing our project code
    start coding.

--- project.py
+++ project.py
@@ -1,1 +1,15 @@
-print("hello world")
+class student:
+    def __init__(self,number, firstName, lastName, group):
+        self.__number = number
+        self.__firstName = firstName
+        self.__lastName = lastName
+        self.__group = group
+
+    def __str__(self) -> str:
+        return str(self.__number) + " | " + self.__firstName + " " + self.__lastName + " | " + str(self.__group) 
+
+st1 = student(1, "abdallah", "ismaili", 101)
+print(st1.__str__())
+
+
+
