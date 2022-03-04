3abeeb453761a43b619e8e39ec02138c75485d72
Abdallah Ismaili <89218851+AbdallahIsmaili@users.noreply.github.com>
2022-03-04 20:05:37 +0000

    Update project.py

--- project.py
+++ project.py
@@ -20,11 +20,23 @@ class student:
     def getGroup(self):
         return self.__group
 
+def addStudents():
+    try:
+        number = int(input('The student special number: '))
+
+    except ValueError as err:
+        print("Wrong character, enter a number.",err)
+        number = int(input('The student special number: '))
+    else:
+        firstName = input("the first name: ")
 
 
-st1 = student(1, "ABDAllah", "ISMAILI", "DD101")
-print(st1.getNumber())
-print(st1.__str__())
+
+# st1 = student(1, "ABDAllah", "ISMAILI", "DD101")
+# print(st1.getNumber())
+# print(st1.__str__())
+
+addStudents()
 
 
 
